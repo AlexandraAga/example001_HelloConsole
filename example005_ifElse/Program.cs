df855888b059cdceaa0d36e8c006a31385aedf3f
sasha <bananov.sasha.gmail.com>
2023-10-04 23:20:19 +0300

    Сделали

--- example005_ifElse/Program.cs
+++ example005_ifElse/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.Write("");
-string
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
